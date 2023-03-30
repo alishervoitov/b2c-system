ba73bfbdb54c7daf39a46a30e5c4f64df7fa9cbb
Alisher <voitovalisher020@gmail.com>
2023-03-30 09:42:51 +0500

    Url

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from account.views import RegistrationAPIView, UserLoginView, UserProfileView, ChangePasswordView
+from account.views import RegistrationAPIView, UserLoginView, UserProfileView, ChangePasswordView, EditProfileView
 
 urlpatterns = [
     path('register/', RegistrationAPIView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('user_profile/', UserProfileView.as_view(), name='user_profile'),
-    (path('change_password/', ChangePasswordView.as_view(), name='change_password')),
+    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('edit-profile/', EditProfileView.as_view(), name='edit-profile'),
 ]
