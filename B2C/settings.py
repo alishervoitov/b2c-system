6e80e094f24b4deec6516f5924bc03d670425275
Alisher <voitovalisher020@gmail.com>
2023-03-31 10:44:07 +0500

    Drf-yasg

--- B2C/settings.py
+++ B2C/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'rest_framework',
+    'drf_yasg',
+    # 'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
--- B2C/urls.py
+++ B2C/urls.py
@@ -16,7 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="B2C API",
+      default_version='v1',
+      description="Swagger docs for REST API",
+      contact=openapi.Contact("B2C <info@B2C.uz>"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('account.urls'))
+    path('auth/', include('account.urls')),
+    path('api/token-obtain-pair-view/', TokenObtainPairView.as_view(), name='tokenobtainpairview'),
+    path('api/refresh-token/', TokenRefreshView.as_view(), name='refreshtoken'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json/', schema_view.without_ui( cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
