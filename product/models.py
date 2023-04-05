9988365827ff166fa16f55bc762f15206ffa949e
Alisher <voitovalisher020@gmail.com>
2023-04-05 10:37:43 +0500

    Product model

--- product/models.py
+++ product/models.py
@@ -19,3 +19,32 @@ COUNTRY_CHOICE = (
     ('TURKMENISTAN', 'turkmenistan'),
     ('TADJIKISTAN', 'tadjikistan'),
 )
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+    name = models.CharField(max_length=150)
+    price = models.FloatField()
+    image = models.ImageField(null=True, blank=True)
+    quantity = models.IntegerField(default=0)
+    medicine_form = models.CharField(max_length=255)
+    country = models.CharField(max_length=50, choices=COUNTRY_CHOICE)
+    company = models.CharField(max_length=255)
+    trade_name_of_the_drug = models.CharField(max_length=50)
+    active_ingredient = models.CharField(max_length=255, null=True, blank=True)
+    composition = models.TextField(max_length=1000)
+    pharmacotherapeutic_group = models.TextField(max_length=5000)
+    contraindication = models.TextField(max_length=5000)
+    pharmacokinetic = models.TextField(max_length=5000)
+    storage_condition = models.TextField(max_length=500)
+    expiration = models.CharField(max_length=255)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def imageURL(self):
+        if self.image:
+            return self.image.url
+        return ''
+
+    def __str__(self):
+        return self.name
