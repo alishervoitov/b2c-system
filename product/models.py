1da30f9bea4364d1e986b133f61d623c3e383d05
Alisher <voitovalisher020@gmail.com>
2023-04-05 09:49:16 +0500

    Category model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from account.models import CustomerUser
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+COUNTRY_CHOICE = (
+    ('UZBEKISTAN', 'uzbekistan'),
+    ('KYRGIZISTAN', 'kyrgizistan'),
+    ('KAZAKHSTAN', 'kazakhstan'),
+    ('TURKMENISTAN', 'turkmenistan'),
+    ('TADJIKISTAN', 'tadjikistan'),
+)
