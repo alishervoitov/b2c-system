2db988103c3584ff158257b3e19a435bf31ee7c1
Alisher <voitovalisher020@gmail.com>
2023-04-05 11:21:59 +0500

    installed apps

--- B2C/settings.py
+++ B2C/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'product',
+    'zakaz',
     'rest_framework',
     'drf_yasg',
     # 'rest_framework_swagger',
