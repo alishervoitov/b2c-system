bc7e1d1505b6c4339c27fc98d17978decaa1ded0
Alisher <voitovalisher020@gmail.com>
2023-04-06 09:45:38 +0500

    Python files

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from product.models import Category, Product, Comment, Rating, RatingStar
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
