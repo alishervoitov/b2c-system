382ab6a1ca56f685da6a28cc70ee08672d4079ad
Alisher <voitovalisher020@gmail.com>
2023-04-08 09:28:00 +0500

    Category url

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from product.views import ProductView, ProductDetailView
+from product.views import ProductView, ProductDetailView, CategoryView
 
 urlpatterns = [
     path('products/', ProductView.as_view(), name='products'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('categories/', CategoryView.as_view(), name='categories'),
 ]
