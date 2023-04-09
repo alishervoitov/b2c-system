98545ff4e81b6e11f455cdce2ed2af2387651351
Alisher <voitovalisher020@gmail.com>
2023-04-09 20:44:31 +0500

    Order models

--- zakaz/models.py
+++ zakaz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from account.models import CustomerUser
+
+class Order(models.Model):
+
+    STATUSES = [
+        ('PENDING', 'pending'),
+        ('INPROGRESS', 'inprogress'),
+        ('COMPLATED', 'complated'),
+        ('CANCELED', 'canceled')
+    ]
+
+    customer = models.ForeignKey(CustomerUser, on_delete=models.SET_NULL, null=True, related_name='orders')
+    total = models.DecimalField(default=0, max_digits=10, decimal_places=2, blank=True, null=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    expired_date = models.DateTimeField(null=True)
+    required_date = models.DateTimeField(null=True)
+    shipped_date = models.DateTimeField(null=True)
+    canceled_date = models.DateTimeField(null=True)
+    status = models.CharField(max_length=10, choices=STATUSES, default='PENDING')
+
+    def __str__(self):
+        return f'{self.customer.__str__()} -> order:{self.id}'
