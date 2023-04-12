2aa459a68fce877a792e0e90038dfd3cf421e871
Alisher <voitovalisher020@gmail.com>
2023-04-12 12:56:24 +0500

    Search filters in cart

--- zakaz/views.py
+++ zakaz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import filters
 from rest_framework import generics, permissions
 
 from zakaz.models import OrderDetail
@@ -9,6 +10,9 @@ class CartItemView(generics.ListAPIView):
 
     serializer_class = CartItemSerializer
     permission_classes = (permissions.IsAuthenticated, )
+    filter_backends = [filters.SearchFilter]
+    search_fields = [
+        'product__name', 'product__description', 'product__category__name']
 
     def get_queryset(self):
         user = self.request.user
